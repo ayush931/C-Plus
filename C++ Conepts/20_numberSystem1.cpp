e6d4376f013c12bf8ae079f10e7616f6f8fff6dd
ayush931 <ayushkumar9315983@gmail.com>
2024-03-06 18:06:06 +0530

    Completition of basic C++ Concepts

---
+++ C++ Conepts/20_numberSystem1.cpp
@@ -0,0 +1,24 @@
+// Convert binary to decimal.
+
+#include<iostream>
+using namespace std;
+
+int main () {
+
+    int n;
+    cin >> n;
+
+    int ans = 0;
+    int power = 1;
+
+    while (n > 0) {
+        int lastDigit = n % 10;
+        ans = ans + (lastDigit * power);
+        power *= 2;
+        n /= 10;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
