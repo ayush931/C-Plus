fa4530ed8b06f2461e0ae7d523177397c7314fc9
ayush931 <ayushkumar9315983@gmail.com>
2024-06-17 09:08:36 +0530

    Array

---
+++ Array/Basic array.cpp
@@ -0,0 +1,10 @@
+// Data structure which stores a collection of homogeneous items like int, char, bool, etc.
+// They have contiguous memory.
+
+#include<iostream>
+using namespace std;
+
+int main () {
+	cout << "Hello";
+	return 0;
+}
