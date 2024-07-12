7a36bf13d4c456189987bc1c09d19e6d38d33df5
ayush931 <ayushkumar9315983@gmail.com>
2024-07-12 13:33:30 +0530

    Revision

---
+++ 2-D Arrays/multiplyMatrices.cpp
@@ -0,0 +1,69 @@
+// Write a program to display multiplication of two matrices entered by the user.
+
+#include<iostream>
+using namespace std;
+
+int main () {
+
+    int row1, column1;
+
+    cout << "Enter the row of Array 1: ";
+    cin >> row1;
+
+    cout << "Enter the column of Array 1: ";
+
+    cin >> column1;
+
+    int array1[row1][column1];
+
+    cout << "Enter the element of Array 1" << endl;
+
+    for (int i = 0; i < row1; i++) {
+        for (int j = 0; j < column1; j++) {
+            cin >> array1[i][j];
+        }
+    }
+
+    int row2, column2;
+
+    cout << "Enter the row of Array 2: ";
+    cin >> row2;
+
+    cout << "Enter the column of Array 2: ";
+    cin >> column2;
+
+    int array2[row2][column2];
+
+    cout << "Enter the element of Array 2" << endl;
+
+    for (int i = 0; i < row2; i++) {
+        for (int j = 0; j < column2; j++) {
+            cin >> array2[i][j];
+        }
+    }
+
+    if (column1 != row2) {
+        cout << "matrix multiplication is not possible for this input";
+    }
+
+    int ansArray[row1][column2];
+
+    for (int i = 0; i < row1; i++) {
+        for (int j = 0; j < column2; j++) {
+            int value = 0;
+            for (int k = 0; k < row2; k++) {
+                value += array1[i][k] * array2[k][j];
+            }
+            ansArray[i][j] = value;
+        }
+    }
+
+    cout << "Multiplication of Array 1 and Array 2: " << endl;
+
+    for (int i = 0; i < row1; i++) {
+        for (int j = 0; j < column2; j++) {
+            cout << ansArray[i][j] << " ";
+        }
+    }
+
+}
---
+++ 2-D Arrays/taking2DArrayInput.cpp
@@ -0,0 +1,41 @@
+/*
+    -> Representing grids
+    -> Faster and simpler access to an element
+    -> Predefined size
+*/
+
+
+#include<iostream>
+using namespace std;
+
+int main () {
+
+    int rows, columns;
+    
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    cout << "Enter the number of columns: ";
+    cin >> columns;
+
+    int array[rows][columns];
+
+    cout << "Enter the element of array: " << endl;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            cin >> array[i][j];
+        }
+    }
+
+    cout << "Your array is: " << endl;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
