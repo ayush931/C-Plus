0f2877be71dbf2b4101a456afe45cce3a3b1574c
ayush931 <ayushkumar9315983@gmail.com>
2024-07-12 10:35:40 +0530

    a

--- Revision.cpp
+++ Revision.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
+void prefixSum (int array[], int size) {
+
+
+
+    for (int i = 0; i < size; i++) {
+        array[i] = array[i - 1] + array[i];
+    }
+}
+
 int main () {
-    
+
+    int array[] = {1, 2, 3, 4, 5};
+    int size = 5;
+
+    prefixSum(array, size);
+
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << endl;
+    }
+
     return 0;
 }
