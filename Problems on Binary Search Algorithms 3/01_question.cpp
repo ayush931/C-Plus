b816d1bd005a71bae6af2cce6d0ad9f13bf04fd6
ayush931 <ayushkumar9315983@gmail.com>
2025-02-28 01:20:25 +0530

    Added revision

---
+++ Problems on Binary Search Algorithms 3/01_question.cpp
@@ -0,0 +1,18 @@
+// You have 'n' (n <= 10^5) boxes of chocolates. Each box contains a[i] (a[i] <= 10000) chocolates. You need to distribute these boxes among 'm' students such that the minimum number of chocolates allocated to a student is minimum.
+// a. One box will be allocated to exactly one student.
+// b. All the boxes should be allocated.
+// c. Each student has to be allocated at least one box.
+// d. Allotment should be in contiguous order, for instance, a student cannot be allocated box 1 and box 3, skipping box 2.
+// Calculate and return that minimum possible number. Assume that it is always possible to distribute the chocolates.
+
+// The first line of input will contain the value of n, the number of boxes.
+// The second line of input will contain the n numbers of denoting the number of chocolates in each box.
+// The third line will contain the m, number of students.
+
+#include <iostream>
+using namespace std; 
+
+int main () {
+
+  return 0;
+}
