da8e7b29301994a825ce887608af5f32b47e34ed
ayush931 <ayushkumar9315983@gmail.com>
2025-03-17 09:34:52 +0530

    Problems on Binary Search Algorithms 3

--- Problems on Binary Search Algorithms 3/01_question.cpp
+++ Problems on Binary Search Algorithms 3/01_question.cpp
@@ -9,10 +9,65 @@
 // The second line of input will contain the n numbers of denoting the number of chocolates in each box.
 // The third line will contain the m, number of students.
 
-#include <iostream>
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
-int main () {
+bool canDistChoc(vector<int> &arr, int mid, int s) {
+  int n = arr.size();
+  int studentsReqd = 1;
+  int currSum = 0;
 
+  for (int i = 0; i < n; i++) {
+    if (arr[i] > mid) {
+      return false;
+    }
+    if (currSum + arr[i] > mid) {
+      studentsReqd++;
+      currSum = arr[i];
+      if (studentsReqd > s)
+        return false;
+    }
+    else {
+      currSum += arr[i];
+    }
+  }
+  return true;
+}
+
+int distChoc(vector<int> &arr, int s) {
+  int n = arr.size();
+  int lo = arr[0];
+  int hi = 0;
+  for (int i = 0; i < arr.size(); i++) {
+    hi += arr[i];
+  }
+  int ans = -1;
+  while (lo <= hi) {
+    int mid = lo + (hi - lo) / 2;
+    if (canDistChoc(arr, mid, s)) {
+      ans = mid;
+      hi = mid - 1;
+    }
+    else {
+      lo = mid + 1;
+    }
+  }
+  return ans;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> v;
+
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    v.push_back(x);
+  }
+
+  int s;
+  cin >> s;
+  cout << distChoc(v, s) << endl;
   return 0;
 }
