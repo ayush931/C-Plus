9c7f83cf5e709b5cf5b8ec2900b9faf142482cd3
ayush931 <ayushkumar9315983@gmail.com>
2025-06-27 22:22:36 +0530

    added the create company name

---
+++ Linked List 2/05_question.cpp
@@ -0,0 +1,9 @@
+// You are given an array of k linked-lists lists, each linked-list is sorted in ascending order. Merge all the linked-lists into one sorted linked-lists and return it.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  
+  return 0;
+}
